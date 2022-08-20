40c3d2e950733e27a9ef9c70da2ba78cc766ee28
Hannele Ruiz <justlemoncl@gmail.com>
2022-08-20 16:51:09 -0400

    Added base class for all of the effects

---
+++ NeonControl/Effects/Effect.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using GTA;
+
+namespace NeonControl.Effects
+{
+    /// <summary>
+    /// Represents
+    /// </summary>
+    public abstract class Effect
+    {
+        #region Properties
+
+        /// <summary>
+        /// If the effect is enabled or not.
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        #endregion
+        
+        #region Functions
+
+        /// <summary>
+        /// Initializes the Effect requirements.
+        /// </summary>
+        public abstract void Initialize();
+        /// <summary>
+        /// Resets the effect of the vehicle.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to reset.</param>
+        public abstract void Reset(Vehicle vehicle);
+        /// <summary>
+        /// Processes the status of the vehicle.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to process.</param>
+        /// <returns>The color that was obtained from the effect.</returns>
+        public abstract Color Process(Vehicle vehicle);
+
+        #endregion
+    }
+}
