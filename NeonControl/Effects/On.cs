17266c9112755b4e1afbaf28199df822eccf1b5c
Hannele Ruiz <justlemoncl@gmail.com>
2022-08-20 16:52:18 -0400

    Added Always On effect

---
+++ NeonControl/Effects/On.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using GTA;
+
+namespace NeonControl.Effects
+{
+    /// <summary>
+    /// Keeps the neon always on.
+    /// </summary>
+    public class On : Effect
+    {
+        /// <inheritdoc/>
+        public override void Initialize()
+        {
+        }
+        /// <inheritdoc/>
+        public override void Reset(Vehicle vehicle)
+        {
+        }
+        /// <inheritdoc/>
+        public override Color Process(Vehicle vehicle) => vehicle.GetBaseColor();
+    }
+}
