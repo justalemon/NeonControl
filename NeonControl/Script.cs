9886b52166e6140dc580efe9031ccdd8bfa0813f
Hannele Ruiz <justlemoncl@gmail.com>
2022-08-20 15:50:09 -0400

    Added tracking of known vehicles

--- NeonControl/Script.cs
+++ NeonControl/Script.cs
@@ -12,9 +12,11 @@ namespace NeonControl
     {
         #region Fields
 
-        private static readonly Dictionary<string, DecoratorType> decorators = new Dictionary<string, DecoratorType>()
+        private static readonly List<Vehicle> knownVehicles = new List<Vehicle>();
+        private static readonly Dictionary<string, DecoratorType> decorators = new Dictionary<string, DecoratorType>
         {
             { "neon_start", DecoratorType.Int },
+            { "neon_controlled", DecoratorType.Bool },
             { "neon_base_r", DecoratorType.Int },
             { "neon_base_g", DecoratorType.Int },
             { "neon_base_b", DecoratorType.Int },
@@ -31,15 +33,39 @@ namespace NeonControl
         {
             Decorators.Initialize();
             Decorators.Register(decorators);
-            Tick += OnTick;
+            Tick += OnInit;
         }
         
         #endregion
         
         #region Events
 
+        private void OnInit(object sender, EventArgs e)
+        {
+            foreach (Vehicle vehicle in World.GetAllVehicles())
+            {
+                if (vehicle.IsKnown())
+                {
+                    knownVehicles.Add(vehicle);
+                }
+            }
+            
+            Tick -= OnInit;
+            Tick += OnTick;
+        }
         private void OnTick(object sender, EventArgs e)
         {
+            Vehicle currentVehicle = Game.Player.Character.CurrentVehicle;
+
+            if (currentVehicle != null && !knownVehicles.Contains(currentVehicle))
+            {
+                if (!currentVehicle.IsKnown())
+                {
+                    currentVehicle.MarkKnown();
+                }
+
+                knownVehicles.Add(currentVehicle);
+            }
         }
 
         #endregion
