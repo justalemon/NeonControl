f38c9b663ddf6b4afd593dd35954747aa3f75499
Hannele Ruiz <justlemoncl@gmail.com>
2022-08-20 12:26:43 -0400

    Added decorator to keep track of the original neon color

---
+++ NeonControl/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using GTA;
+using GTA.Native;
+
+namespace NeonControl
+{
+    /// <summary>
+    /// Some extensions to make working with vehicles easier.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Gets the original color of the neon.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to get the neon conor from.</param>
+        /// <returns>The original neon color.</returns>
+        public static Color GetBaseColor(this Vehicle vehicle)
+        {
+            int r = Function.Call<int>(Hash.DECOR_GET_INT, vehicle, "neon_base_r");
+            int g = Function.Call<int>(Hash.DECOR_GET_INT, vehicle, "neon_base_g");
+            int b = Function.Call<int>(Hash.DECOR_GET_INT, vehicle, "neon_base_b");
+            return Color.FromArgb(r, g, b);
+        }
+        /// <summary>
+        /// Sets the original neon color.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to set the neon color.</param>
+        /// <param name="color">The color to set.</param>
+        public static void SetBaseColor(this Vehicle vehicle, Color color)
+        {
+            Function.Call<bool>(Hash.DECOR_SET_INT, vehicle, "neon_base_r", color.R);
+            Function.Call<bool>(Hash.DECOR_SET_INT, vehicle, "neon_base_g", color.G);
+            Function.Call<bool>(Hash.DECOR_SET_INT, vehicle, "neon_base_b", color.B);
+        }
+    }
+}
--- NeonControl/Script.cs
+++ NeonControl/Script.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using GTA;
+using GTA.Native;
 
 namespace NeonControl
 {
+    /// <summary>
+    /// The Script that controls the Neon effects.
+    /// </summary>
     public class NeonControl : Script
     {
         #region Constructors
 
-        public NeonControl()
+        /// <summary>
+        /// Creates a new Neon control script.
+        /// </summary>
+        public unsafe NeonControl()
         {
+            IntPtr addr = Game.FindPattern("\x40\x53\x48\x83\xEC\x20\x80\x3D\x00\x00\x00\x00\x00\x8B\xDA\x75\x29", "xxxxxxxx????xxxxx");
+            if (addr == IntPtr.Zero)
+            {
+                throw new DataMisalignedException("Memory pattern was not found.");
+            }
+            
+            byte* decoratorLock = (byte*)(addr + *(int*)(addr + 8) + 13);
+            *decoratorLock = 0;
+
+            Function.Call(Hash.DECOR_REGISTER, "neon_base_r", 3);
+            Function.Call(Hash.DECOR_REGISTER, "neon_base_g", 3);
+            Function.Call(Hash.DECOR_REGISTER, "neon_base_b", 3);
+            
+            *decoratorLock = 1;
+            
             Tick += OnTick;
         }
         
