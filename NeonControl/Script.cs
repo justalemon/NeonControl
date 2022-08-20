e5acec8f9290a27c5d58b744bd12f25555afc391
Hannele Ruiz <justlemoncl@gmail.com>
2022-08-20 12:04:52 -0400

    Added tick and constructor

--- NeonControl/Script.cs
+++ NeonControl/Script.cs
@@ -1,8 +1,25 @@
-﻿using GTA;
+﻿using System;
+using GTA;
 
 namespace NeonControl
 {
     public class NeonControl : Script
     {
+        #region Constructors
+
+        public NeonControl()
+        {
+            Tick += OnTick;
+        }
+        
+        #endregion
+        
+        #region Events
+
+        private void OnTick(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
     }
 }
